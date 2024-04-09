d7b4404e030b9f5b0d6a166abfb0a706a2af9e2c
Vaso <koupetsid@csd.auth.gr>
2024-04-09 16:46:43 +0300

    Zari

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hiiii");
+        Student s1=new Student("Marina Satti",25);
+        System.out.println(s1.getName() + " : " + s1.getAge());
     }
 }
