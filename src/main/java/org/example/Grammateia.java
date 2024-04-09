a5f65a6942b8750a0963d463ec949619511d4697
BozatzidisLeonidas <bozatzidisleonidas@gmail.com>
2024-04-09 13:00:08 +0300

    Attributes in Grammateia

--- src/main/java/org/example/Grammateia.java
+++ src/main/java/org/example/Grammateia.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Grammateia {
+    private String name;
+    private int age;
+
+    public Grammateia (String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+
 }
